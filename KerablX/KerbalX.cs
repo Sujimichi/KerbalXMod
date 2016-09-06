4619f75ed5fb53ad48925ae7ac917f25cba60a2d
Sujimichi <sujimichi@gmail.com>
2016-09-07 00:19:55 +0100

    working on roughing out API actions for username/password and token login

--- KerablX/KerbalX.cs
+++ KerablX/KerbalX.cs
@@ -3,48 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using KerablXWindow;
+//using UnityEngine.UI;
+//using UnityEngine.EventSystems;
+
 
 namespace KerbalX
 {
+	public class KerbalX
+	{
+		public static List<string> log_data = new List<string>();
+		public static string notification = "";
+		public static bool logged_in = false;
+
+		public static void log (string e){ log_data.Add (e); }
+		public static string last_log()
+		{
+			if(log_data.Count != 0){
+				return log_data [log_data.Count - 1];
+			}else{
+				return "nothing logged yet";
+			}
+		}
+		public static void show_log(){
+			foreach (string l in log_data) { Debug.Log ("[KerbalX] " + l); }
+		}
+		public static void notify(string s){
+			notification = s;
+			log (s);
+		}
+		public static void load_token(){
+			//attempt to read the token file from the root of KSP
+			//read token file if present
+			KerbalX.notify("Reading token from file");
+			KerbalXAPI.authenticate_token ("bob's pyjamas");
+		}
+
+	}
+
+	public class KerbalXAPI
+	{
+		public static string token = null; 
+
+		public static void set_token(string new_token){
+			token = new_token;
+			//and write token to KerablX.key in KSP root dir
+		}
+
+		public static bool authenticate_token(string new_token){
+			//make request to site to authenticate token and return true or false
+			KerbalX.notify("Authenticating token with KerbalX.com...");
+			if(new_token == "bob's pyjamas"){
+				token = new_token;
+				KerbalX.logged_in = true;
+				KerbalX.notify("logged in");
+				return true;
+			}else{				
+				token = null;
+				KerbalX.logged_in = false;
+				KerbalX.notify("token was invalid");
+				return false;
+			}
+		}
+
+		public static void login(string username, string password){
+			//make request to site to authenticate username and password and get token back
+			KerbalX.notify("loging into KerbalX.com...");
+
+			//succeed
+			set_token("lsjdlkfjslkdjflskjdflksjdfl"); //will be token returned from site
+			KerbalX.logged_in = true;
+			KerbalX.notify("login succsessful, yay!");
+		}
+
+	}
 
 	[KSPAddon(KSPAddon.Startup.MainMenu, false)]
 	public class KerbalXLoginWindow : KerbalXWindow
 	{
-		private string output_label = "some stuff";
+
 		private string username = "";
 		private string password = "";
 
+
 		private void Start(){
 			window_title = "KerbalX::Login";
-			window_pos = new Rect(100,100, 310, 200);
+			window_pos = new Rect((Screen.width/2 - 310/2),100, 310, 5);
+			KerbalX.log ("starting");
+			KerbalX.load_token ();
 		}
 
 		protected override void WindowContent(int win_id)
 		{
-			grid (310f, window => {
-				GUILayout.Label ("username", GUILayout.Width (60f));
-				username = GUILayout.TextField (username, 255, GUILayout.Width (250f));
-			});
+			if(KerbalX.logged_in == false){					
+				grid (310f, window => {
+					GUILayout.Label ("username", GUILayout.Width (60f));
+					username = GUILayout.TextField (username, 255, GUILayout.Width (250f));
+				});
 
-			grid (310f, window => {
-				GUILayout.Label ("password", GUILayout.Width(60f));
-				password = GUILayout.TextField (password, 255, GUILayout.Width(250f));
-			});
+				grid (310f, window => {
+					GUILayout.Label ("password", GUILayout.Width(60f));
+					password = GUILayout.TextField (password, 255, GUILayout.Width(250f));
+				});
+			}
 
-			GUILayout.Label (output_label, GUILayout.Width(310f));
+			GUILayout.Label (KerbalX.notification, GUILayout.Width(310f));
 
-			if (GUILayout.Button ("Login")) {
-				output_label = "checking authorization";
-				window_pos = new Rect(400,400, 310, 200);
+			if (KerbalX.logged_in == false) {
+				if (GUILayout.Button ("Login")) {
+					
+					KerbalXAPI.login (username, password);
+				}
+			}else{
+				if (GUILayout.Button ("Log out")) {
+					KerbalX.logged_in = false;
+					KerbalX.notify ("logged out");
+				}				
 			}
 		}
 	}
 
 
-	[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+	[KSPAddon(KSPAddon.Startup.EveryScene, false)]
 	public class KXTest : KerbalXWindow
 	{
-
 		private void Start()
 		{
 			window_title = "test window";
@@ -52,110 +134,14 @@ namespace KerbalX
 
 		protected override void WindowContent(int win_id)
 		{
-			GUILayout.Label ("some nonsense", GUILayout.Width (60f));
-			grid (310f, window => {
-				GUILayout.Label ("username", GUILayout.Width (60f));
-				GUILayout.Label ("height " + window.height);
+			grid (300f, window => {
+				GUILayout.Label (KerbalX.last_log ());
 			});
-		}
 
+			if (GUILayout.Button ("print log to console")) {
+				KerbalX.show_log ();
+			}
+		}
 	}
-
-	/* KerbalXWindow is a base class to be inherited by classes which draw GUI windows
-	It provides common setup required to draw a GUI window, enabling DRY and minimal window classes.
-	A class which inherits KerbalXWindow needs to override the WindowContent method to define the content of the window
-	Basic Usage:
-		[KSPAddon(KSPAddon.Startup.MainMenu, false)]
-		public class SimpleWindow : KerbalXWindow
-		{
-			protected override void WindowContent(int win_id)
-			{
-				GUILayout.Label ("some nonsense", GUILayout.Width (60f));
-			}
-		}
-	Attributes like the window title, size/pos, id are set, but can be overridden by defining a Start() method
-	Configured Usage:
-		[KSPAddon(KSPAddon.Startup.MainMenu, false)]
-		public class SimpleWindow : KerbalXWindow
-		{
-			private void Start()
-			{
-				window_pos = new Rect(100,100,500,200); //defaults to new Rect() if not set
-				window_title = "test window";			//defaults to "untitled window" if not set
-				window_id = 42;							//defaults to the next ID in sequence (change last_window_id in the base class to change the sequence start point)
-				footer = false							//defaults to true. if true adds a common set of GUI elements defined in DrawWindow
-				draggable = false;						//defaults to true. makes the windows draggable, duh. 
-			}
-
-			protected override void WindowContent(int win_id)
-			{
-				GUILayout.Label ("some nonsense", GUILayout.Width (60f));
-			}
-		}	 
-	KerbalXWindow also provides the handy-dandy fabtastic grid method. grid takes a width and a lambda (delegate) statement and wraps the actions defined in the
-	lambda in calls to BeginHorizontal and EndHorizontal.  This ensures End is always called after a begin, and (I think) makes for clearer and more readable code.
- 	*/
-	public class KerbalXWindow : MonoBehaviour
-	{
-		protected Rect window_pos = new Rect();
-		protected string window_title = "untitled window";
-		protected int window_id = 0;
-		protected bool footer = true;
-		protected bool draggable = true;
-		static int last_window_id = 0;
-
-		//Definition of delegate to be passed into the grid method 
-		protected delegate void Content(Rect win);
-
-		/* Essentially wraps the function of a delegate in calls to BeginHorizontal and EndHorizontal
-		Takes a width float and a delegate/statement lambda and passes the width float to the call to BeginHorizontal 
-		then calls the delegate and lastly calls EndHorizontal
-		Usage:
-			grid (width, window => {
-				// Calls to draw GUI elements inside a BeginHorizontal group ie;
-				// GUILayout.Label ("some nonsense", GUILayout.Width (60f));
-				//window is passed in as the Rect for the window position/size
-			});	
-		*/
-		protected void grid(float width, Content con)
-		{
-			GUILayout.BeginHorizontal(GUILayout.Width(width));
-			con(window_pos);
-			GUILayout.EndHorizontal ();
-		}
-
-		//called on each frame, handles drawing the window and will assign the next window id if it's not set
-		protected void OnGUI()
-		{
-			if(window_id == 0){
-				window_id = last_window_id + 1;
-				last_window_id = last_window_id + 1;
-			}
-			window_pos = GUILayout.Window (window_id, window_pos, DrawWindow, window_title);
-		}
-
-		//Callback methods which is passed to GUILayout.Window in OnGUI.  Calls WindowContent and performs common window actions
-		private void DrawWindow(int window_id)
-		{
-			WindowContent (window_id);	//Draw the main content of the window as defined by WindowContent
-			if(footer){
-				GUILayout.Label ("rendered with a moose");
-				GUILayout.Label ("window_id: " + window_id);				
-			}
-			if(draggable){
-				GUI.DragWindow();
-			}
-		}
-
-		//The main method which defines the content of a window.  This method is provided so as to be overridden in inherited classes
-		protected virtual void WindowContent(int window_id)
-		{
-			
-		}
-
-		private void onDestroy(){
-			print ("shit was destroyed yo"); 
-		}
-
-	}
+		
 }
---
+++ KerablX/KerbalXWindow.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+
+namespace KerablXWindow
+{
+	/* KerbalXWindow is a base class to be inherited by classes which draw GUI windows
+	It provides common setup required to draw a GUI window, enabling DRY and minimal window classes.
+	A class which inherits KerbalXWindow needs to override the WindowContent method to define the content of the window
+	Basic Usage:
+		[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+		public class SimpleWindow : KerbalXWindow
+		{
+			protected override void WindowContent(int win_id)
+			{
+				GUILayout.Label ("some nonsense", GUILayout.Width (60f));
+			}
+		}
+	Attributes like the window title, size/pos, id are set, but can be overridden by defining a Start() method
+	Configured Usage:
+		[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+		public class SimpleWindow : KerbalXWindow
+		{
+			private void Start()
+			{
+				window_pos = new Rect(100,100,500,200); //defaults to new Rect() if not set
+				window_title = "test window";			//defaults to "untitled window" if not set
+				window_id = 42;							//defaults to the next ID in sequence (change last_window_id in the base class to change the sequence start point)
+				footer = false							//defaults to true. if true adds a common set of GUI elements defined in DrawWindow
+				draggable = false;						//defaults to true. makes the windows draggable, duh. 
+			}
+
+			protected override void WindowContent(int win_id)
+			{
+				GUILayout.Label ("some nonsense", GUILayout.Width (60f));
+			}
+		}	 
+	KerbalXWindow also provides the handy-dandy fabtastic grid method. grid takes a width and a lambda (delegate) statement and wraps the actions defined in the
+	lambda in calls to BeginHorizontal and EndHorizontal.  This ensures End is always called after a begin, and (I think) makes for clearer and more readable code.
+ 	*/
+	public class KerbalXWindow : MonoBehaviour
+	{
+		protected Rect window_pos = new Rect();
+		protected string window_title = "untitled window";
+		protected int window_id = 0;
+		protected bool footer = true;
+		protected bool draggable = true;
+		static int last_window_id = 0;
+
+		//Definition of delegate to be passed into the grid method 
+		protected delegate void Content(Rect win);
+
+		/* Essentially wraps the function of a delegate in calls to BeginHorizontal and EndHorizontal
+		Takes a width float and a delegate/statement lambda and passes the width float to the call to BeginHorizontal 
+		then calls the delegate and lastly calls EndHorizontal
+		Usage:
+			grid (width, window => {
+				// Calls to draw GUI elements inside a BeginHorizontal group ie;
+				// GUILayout.Label ("some nonsense", GUILayout.Width (60f));
+				//window is passed in as the Rect for the window position/size
+			});	
+		*/
+		protected void grid(float width, Content con)
+		{
+			GUILayout.BeginHorizontal(GUILayout.Width(width));
+			con(window_pos);
+			GUILayout.EndHorizontal ();
+		}
+
+		//called on each frame, handles drawing the window and will assign the next window id if it's not set
+		protected void OnGUI()
+		{
+			if(window_id == 0){
+				window_id = last_window_id + 1;
+				last_window_id = last_window_id + 1;
+			}
+			window_pos = GUILayout.Window (window_id, window_pos, DrawWindow, window_title,  GUILayout.Width( window_pos.width ), GUILayout.ExpandHeight (true));
+		}
+
+		//Callback methods which is passed to GUILayout.Window in OnGUI.  Calls WindowContent and performs common window actions
+		private void DrawWindow(int window_id)
+		{
+			WindowContent (window_id);	//Draw the main content of the window as defined by WindowContent
+			if(footer){
+				GUILayout.Label ("kerablx.com");
+			}
+			if(draggable){
+				GUI.DragWindow();
+			}
+		}
+
+		//The main method which defines the content of a window.  This method is provided so as to be overridden in inherited classes
+		protected virtual void WindowContent(int window_id)
+		{
+
+		}
+
+		private void onDestroy(){
+			print ("shit was destroyed yo"); 
+		}
+
+	}
+}
+
