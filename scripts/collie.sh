f6f0f5da30a9b48f345076b2eca915b012ba8d65
Sujimichi <sujimichi@gmail.com>
2016-10-16 19:00:06 +0100

    update build script to include licence and create zip

--- scripts/collie.sh
+++ scripts/collie.sh
@@ -1,6 +1,7 @@
 #Collie - rounds all the files up into release
 
 rm -rf bin/Release/KerbalX
+rm bin/Release/KerbalXMod.zip
 
 mkdir bin/Release/KerbalX -p
 mkdir bin/Release/KerbalX/Plugins -p
@@ -8,6 +9,11 @@ mkdir bin/Release/KerbalX/Assets -p
 
 cp bin/Release/KerbalX.dll bin/Release/KerbalX/Plugins/KerbalX.dll
 cp -a assets/images/*.png bin/Release/KerbalX/Assets/
+cp LICENCE.txt bin/Release/KerbalX/LICENCE.txt
 
 rm bin/Release/KerbalX.dll
 rm bin/Release/KerbalX.dll.mdb
+
+cd bin/Release
+zip -r KerbalXMod.zip KerbalX/
+
